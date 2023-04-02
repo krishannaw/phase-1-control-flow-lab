c27ec6af417b4d5f543d0a8bc6c077cae10c0093
Krishanna Williams <krishannaw@gmail.com>
2023-04-02 18:36:36 -0500

    control

--- index.js
+++ index.js
@@ -1,11 +1,66 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance){
+    if (distance <= 400) {
+      return 'This one is on me!';
+    } else {
+      return 'You have to pay for this ride.';
+    }
+  }
+  function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
+      return 'This one is on me!';
+    } else if (distance <= 2000) {
+      return 'That will be twenty bucks.';
+    } else {
+      return 'You have to pay for this ride.';
+    }
+  }
+  function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
+      return 'This one is on me!';
+    } else if (distance <= 2000) {
+      return 'That will be twenty bucks.';
+    } else {
+      return 'I will gladly take your thirty bucks.';
+    }
+  }
+  function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
+      return 'This one is on me!';
+    } else if (distance <= 2000) {
+      return 'That will be twenty bucks.';
+    } else if (distance <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else {
+      return 'No can do.';
+    }
+  }
+  function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : undefined;
+  }
+  function ternaryCheckCity(destinationCity) {
+    if (destinationCity === 'NYC') {
+      return 'Ok, sounds good.';
+    } else {
+      return 'No go.';
+    }
+  }
+  function switchOnCharmFromTip(tip) {
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
+  }
+  function switchOnCharmFromTip(tip) {
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
+  }
